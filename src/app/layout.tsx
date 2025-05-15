4e222a57f2b5f99bea521a05d1b4b94f00c1ec29
Luis Castillo <luisestebancastillopedroza90@gmail.com>
2025-05-15 17:25:17 +0000

    ponlo en mayusculas

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
 
 export const metadata: Metadata = {
-  title: 'Viralitycs',
+  title: 'VIRALITYCS',
   description: 'Aplicación Web Interactiva para Modelar la Propagación de Rumores',
 };
 
